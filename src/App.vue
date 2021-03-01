92f57f34d75fd31bce05ed6510d84390bcea6a6b
pavankumarhb <hpavanb1998@gmail.com>
2021-03-01 15:56:11 +0530

    router-task-by-janardhan

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/bitcoin.jpg"><br>
+    <button class="btn btn-primary"><router-link :to="{ name: 'Hello' }">Home</router-link></button>
+    <button class="btn btn-primary"><router-link to="/about">About</router-link></button><br><br>
+    <button class="btn btn-primary"><router-link to="/ethereum">Ethereum</router-link></button>
+   <button class="btn btn-primary"><router-link to="/bitcoin">Bitcoin</router-link></button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  data () {
+    return {
+      msg: 'Welcome to Crypto Currency Info'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1{
+  font-weight: strong;
+  color: #ad243f
+}
+
+</style>
